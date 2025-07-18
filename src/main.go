8dcef547fff005a6c6a57cad3a6a9f45e2e7c7da
AriruaR <ariruarriuar@gmail.com>
2025-07-19 02:10:57 +0300

    123

--- src/main.go
+++ src/main.go
@@ -23,6 +23,30 @@ func main() {
 	artist.Alignment = fyne.TextAlignCenter
 	artist.TextSize = 15
 
+	musicData := []string{
+		"Song 1",
+		"Song 2",
+		"Song 3",
+		"Song 4",
+		"Song 5",
+		"Song 6",
+		"Song 7",
+		"Song 8",
+		"Song 9",
+		"Song 10",
+		"Song 11",
+		"Song 12",
+		"Song 13",
+		"Song 14",
+	}
+	currentSong := 0
+
+	updateSong := func() {
+		title.Text = musicData[currentSong]
+		title.Refresh()
+		// Здесь можно обновлять artist.Text, если есть данные об исполнителе
+	}
+
 	rectangle := canvas.NewRectangle(color.White)
 	rectangle.SetMinSize(fyne.NewSize(100, 100))
 	shadow := canvas.NewRectangle(color.RGBA{255, 248, 220, 255})
@@ -35,8 +59,18 @@ func main() {
 	musicCover := container.New(layout.NewCenterLayout(), shadowContainer)
 
 	playBtn := widget.NewButton("PLAY", func() {})
-	backBtn := widget.NewButton("<=", func() {})
-	nextBtn := widget.NewButton("=>", func() {})
+	backBtn := widget.NewButton("<=", func() {
+		if currentSong > 0 {
+			currentSong--
+			updateSong()
+		}
+	})
+	nextBtn := widget.NewButton("=>", func() {
+		if currentSong < len(musicData)-1 {
+			currentSong++
+			updateSong()
+		}
+	})
 	playBtn.Resize(fyne.NewSize(150, 50))
 	btnContainer := container.NewCenter(
 		container.NewHBox(backBtn, playBtn, nextBtn),
@@ -62,22 +96,6 @@ func main() {
 		timeRow,
 		btnContainer,
 	)
-	musicData := []string{
-		"Song 1",
-		"Song 2",
-		"Song 3",
-		"Song 4",
-		"Song 5",
-		"Song 6",
-		"Song 7",
-		"Song 8",
-		"Song 9",
-		"Song 10",
-		"Song 11",
-		"Song 12",
-		"Song 13",
-		"Song 14",
-	}
 
 	musicList := widget.NewList(
 		func() int { return len(musicData) },
@@ -88,17 +106,17 @@ func main() {
 	)
 	musicList.Resize(fyne.NewSize(100, musicList.Size().Height))
 	musicList.OnSelected = func(id widget.ListItemID) {
-		selectedSong := musicData[id]
-		title.Text = selectedSong
+		currentSong = id
+		updateSong()
 	}
 	contentContainer := container.NewGridWithColumns(2, musicList, container.NewCenter(content))
-	// Накладываем контент на фо
 	mainContainer := container.NewStack(
 		background,
 		contentContainer,
 	)
 
 	w.SetContent(mainContainer)
+	updateSong()
 
 	w.ShowAndRun()
 }
